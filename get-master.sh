4fc2b67e351d6d28d5e2c004c62a33c73cd50945
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-04-20 15:07:02 +0200

    fixes

--- get-master.sh
+++ get-master.sh
@@ -2,5 +2,5 @@
 
 SPARK_HOST=spark.service.consul
 SPARK_PORT=$(dig +short $SPARK_HOST SRV | awk '{print $3}' | sort | head -1) 
-export SPARK_MASTER=mesos://$SPARK_HOST:$SPARK_PORT
+echo mesos://$SPARK_HOST:$SPARK_PORT
 
