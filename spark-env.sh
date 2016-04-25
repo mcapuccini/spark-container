976c6b052c4cb8cce0cf25c41793173d0f0708c6
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-04-25 16:33:57 +0200

    inject secret in local conf

--- spark-env.sh
+++ spark-env.sh
@@ -6,7 +6,7 @@ MANTL_API_MESOS_SECRET_PATH=/etc/sysconfig/mantl-api
 if [ -f "$MANTL_API_MESOS_SECRET_PATH" ];
 then
    MANTL_API_MESOS_SECRET=$(cat $MANTL_API_MESOS_SECRET_PATH) 
-   echo "spark.mesos.principal=mantl-api" >> /opt/spark/dist/conf/spark-defaults.conf
-   echo "spark.mesos.secret=$MANTL_API_MESOS_SECRET" >> /opt/spark/dist/conf/spark-defaults.conf
+   echo "spark.mesos.principal=mantl-api" >> ./spark-defaults.conf
+   echo "spark.mesos.secret=$MANTL_API_MESOS_SECRET" >> ./spark-defaults.conf
 fi
 
