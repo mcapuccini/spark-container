30cf684ee52eb4aa2dd42c6cdf1b35772de90b8c
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-04-25 16:42:31 +0200

    fix directory

--- spark-env.sh
+++ spark-env.sh
@@ -6,7 +6,8 @@ MANTL_API_MESOS_SECRET_PATH=/etc/sysconfig/mantl-api
 if [ -f "$MANTL_API_MESOS_SECRET_PATH" ];
 then
    MANTL_API_MESOS_SECRET=$(cat $MANTL_API_MESOS_SECRET_PATH) 
-   echo "spark.mesos.principal=mantl-api" >> ./spark-defaults.conf
-   echo "spark.mesos.secret=$MANTL_API_MESOS_SECRET" >> ./spark-defaults.conf
+   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+   echo "spark.mesos.principal=mantl-api" >> $DIR/spark-defaults.conf
+   echo "spark.mesos.secret=$MANTL_API_MESOS_SECRET" >> $DIR/spark-defaults.conf
 fi
 
